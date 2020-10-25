902a1ea4b7d7f93fd3a7d1a27fc1c24d4f4d0c57
Brenden Davidson <davidson.brenden15@gmail.com>
2020-10-24 22:45:54 -0500

    Added LGPL license header to source files

---
+++ build-release/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/bdavidson/Projects/libpgs/test
+# Build directory: /home/bdavidson/Projects/libpgs/build-release/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(PgsTest "/home/bdavidson/Projects/libpgs/build-release/test/pgs-test" "WORKING_DIR" "/home/bdavidson/Projects/libpgs/build-release/test/")
+set_tests_properties(PgsTest PROPERTIES  _BACKTRACE_TRIPLES "/home/bdavidson/Projects/libpgs/test/CMakeLists.txt;22;add_test;/home/bdavidson/Projects/libpgs/test/CMakeLists.txt;0;")
